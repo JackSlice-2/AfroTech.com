fc513ad23a95f8e552d1e8dc5cac1d6b7e94be11
Jack Slice <jackslice9@gmail.com>
2023-08-06 18:27:57 -0300

    Extra import

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Who from "./components/Who"
 import Works from "./components/Works"
-import Navbar from "./components/Navbar";
 
 const Container = styled.div`
 height: 100vh;
